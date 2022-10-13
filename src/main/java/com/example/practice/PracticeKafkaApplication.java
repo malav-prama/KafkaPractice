067d6aa73c1b8f4b3ede84554b91b31d965f5f14
malav-prama <101901228+malav-prama@users.noreply.github.com>
2022-10-13 08:50:06 -0700

    Update PracticeKafkaApplication.java

--- src/main/java/com/example/practice/PracticeKafkaApplication.java
+++ src/main/java/com/example/practice/PracticeKafkaApplication.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = "com.example.practice.mongo")
-@EnableJpaRepositories(basePackages =  "com.example.practice.repository")
 public class PracticeKafkaApplication {
 
 	public static void main(String[] args) {
